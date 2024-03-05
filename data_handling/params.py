c9c43ee964b4c4a11fea8e6597a0ebd9ee7b4e92
Pepe Araya <pepearayao@gmail.com>
2024-03-05 15:59:42 +0100

    Created data handling package. Now is loading data and saving in local but will change everything to Classes to is more optimal

---
+++ data_handling/params.py
@@ -0,0 +1,11 @@
+import os
+
+DATA_SIZE = os.environ.get("DATA_SIZE")
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
+GCP_REGION = os.environ.get("GCP_REGION")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_REGION = os.environ.get("BQ_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
